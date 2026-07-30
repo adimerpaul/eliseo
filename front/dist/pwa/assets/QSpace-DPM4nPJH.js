68dd13400b03cc92fa047c15782d6f7b88857f0e
adimerpaul <adimer101@gmail.com>
2026-07-30 05:24:54 -0400

    Funcionando login

--- front/dist/pwa/assets/QSpace-DPM4nPJH.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:front/dist/pwa/assets/QSpace-DAXXMTJl.js
-import{v as a,j as s}from"./index-BZE9Tou1.js";const p=a({name:"QSpace",setup(){const e=s("div",{class:"q-space"});return()=>e}});export{p as Q};
-========
-import{v as a,j as s}from"./index-D-WgUYIN.js";const p=a({name:"QSpace",setup(){const e=s("div",{class:"q-space"});return()=>e}});export{p as Q};
->>>>>>>> acce4f4d029de68a423bc05d26c47a79c469136f:front/dist/pwa/assets/QSpace-DPM4nPJH.js
